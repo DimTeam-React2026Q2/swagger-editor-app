d9f29b75883ff6e3770e6fe0fefb99df215b9545
Mark Pribylnov <mark.pribylnov@gmail.com>
2026-06-27 21:01:04 +0300

    feat: Add Header UI (closes #24) (#26)
    * feat: add Header component to layout
    * refactor: make text bigger for all buttons

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ReactElement } from "react";
 import "@/app/globals.css";
+import Header from "@/ui/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +33,10 @@ export default async function RootLayout({
       lang={locale}
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">
+        <Header isLoggedin={false} />
+        {children}
+      </body>
     </html>
   );
 }
